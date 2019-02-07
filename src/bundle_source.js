df849c91ca91c1b20ee337601d595bac83baadce
Joerg Schneider <joerg.schneider@z-lab.com>
2019-02-07 10:45:15 +0100

    added eslint with airbnb config + prettify plugin for cq

--- src/bundle_source.js
+++ src/bundle_source.js
@@ -3,6 +3,7 @@
  *
  * import all relevant scripts here
  */
-import "./bundle_source_styles.scss";
+import './bundle_source_styles.scss';
 
-console.log("this will the main js bundle entry point.");
+// eslint-disable-next-line
+console.log('this will the main js bundle entry point.');
--- webpack-config/webpack.config.bundle.js
+++ webpack-config/webpack.config.bundle.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-//Paths
-const PROJECT_ROOT = path.resolve(__dirname, "../");
-const PATH_ENTRY = path.join(PROJECT_ROOT, "src/");
-const PATH_OUTPUT = path.join(PROJECT_ROOT, "dist/bundled/");
+// Paths
+const PROJECT_ROOT = path.resolve(__dirname, '../');
+const PATH_ENTRY = path.join(PROJECT_ROOT, 'src/');
+const PATH_OUTPUT = path.join(PROJECT_ROOT, 'dist/bundled/');
 
-//misc vars
-const devMode = process.env.NODE_ENV !== "production";
+// misc vars
+const devMode = process.env.NODE_ENV !== 'production';
 
-//TODO:  add compression plugin, minify css files
+// TODO:  add compression plugin, minify css files
 
 module.exports = {
-  mode: devMode ? "development" : "production",
+  mode: devMode ? 'development' : 'production',
   entry: {
     // split entrypoints and add *.min.js for minimization
 
     // Scripts --------------------------------------------
     // for unminified output
-    "scripts/zeppelin-element-library.js": path.join(
+    'scripts/zeppelin-element-library.js': path.join(
       PATH_ENTRY,
-      "bundle_source.js"
+      'bundle_source.js'
     ),
 
-    //for minified output
-    "scripts/zeppelin-element-library.min.js": path.join(
+    // for minified output
+    'scripts/zeppelin-element-library.min.js': path.join(
       PATH_ENTRY,
-      "bundle_source.js"
+      'bundle_source.js'
     )
   },
   output: {
     // dynamic naming
-    filename: "[name]",
+    filename: '[name]',
     path: PATH_OUTPUT
   },
 
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles/zeppelin-element-library.css"
+      filename: 'styles/zeppelin-element-library.css'
     })
   ],
 
@@ -49,11 +49,11 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: { url: false, sourceMap: true }
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: { sourceMap: true }
           }
         ]
@@ -68,9 +68,9 @@ module.exports = {
         parallel: true,
         sourceMap: true, // set to true if you want JS source maps
 
-        //don't minify zeppelin-element-library.js
+        // don't minify zeppelin-element-library.js
         chunkFilter: chunk =>
-          chunk.name !== "scripts/zeppelin-element-library.js"
+          chunk.name !== 'scripts/zeppelin-element-library.js'
       })
     ]
   }
