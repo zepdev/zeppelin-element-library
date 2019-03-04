3a014e7d0ff89e5a71bc421d02bab7c8c9388a09
Joerg Schneider <joerg.schneider@z-lab.com>
2019-03-04 16:30:43 +0100

    added gzip compression for bundled files in bundlehandler.sh

--- bundlehandler.sh
+++ bundlehandler.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# create bundle folder if not exists
+# create bundle folders if not exist
 mkdir -p bundle;
+mkdir -p bundle/assets/fonts;
 
 #create target destination variable
 TARGET_FILE="bundle/zeppelin-element-library.css"
@@ -9,5 +10,17 @@ TARGET_FILE="bundle/zeppelin-element-library.css"
 # copy bundled css file from react app to bundle folder
 find build/static/css -name \*.css -exec cp {} $TARGET_FILE \;
 
+# copy fonts into bundle folder
+cp -r build/static/media/* bundle/assets/fonts 
+
 # delete sourceMapping comment line from css file
 sed -i '' 's@/\*.*\*/@@g' $TARGET_FILE 
+
+# change 
+sed -i '' 's#/static/media#assets/fonts#g'  $TARGET_FILE 
+
+# gzip files
+#find bundle -type f -name "*.css*" -or -name "*.min.js*" -exec sh -c "gzip < {} > {}.gz" \;
+#find bundle -type f -name "*.css" -o -name "*.min.js" -exec sh -c "gzip < {} > {}.gz" \;
+
+for file in bundle/*.min.js bundle/*.css; do gzip -k $file; done
