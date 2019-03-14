68b5269e780d44f499afe2235bebb8223e43af29
Joerg Schneider <joerg.schneider@z-lab.com>
2019-03-14 12:32:49 +0100

    fix(core): bugfix syntaxerror sed command in bundlehandler.sh

--- bundlehandler.sh
+++ bundlehandler.sh
@@ -13,11 +13,18 @@ find build/static/css -name \*.css -exec cp {} $TARGET_FILE \;
 # copy fonts into bundle folder
 cp -r build/static/media/* bundle/assets/fonts 
 
-# delete sourceMapping comment line from css file
-sed -i '' 's@/\*.*\*/@@g' $TARGET_FILE 
 
-# change 
-sed -i '' 's#/static/media/#assets/fonts/#g'  $TARGET_FILE 
+### TODO: For local use on macOS, please install gnu-sed first!
+# brew install gnu-sed and if you need to use it as "sed", you can add a "gnubin" directory
+# to your PATH from your bashrc like: PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+
+# delete sourceMapping comment line from css file
+sed -i 's@/\*.*\*/@@g' $TARGET_FILE 
+
+# change fonts path in css files
+sed -i 's#/static/media/#assets/fonts/#g' $TARGET_FILE 
+
+##########################################################
 
 # gzip files
 for file in bundle/*.min.js bundle/*.css; do gzip -k -f $file; done
