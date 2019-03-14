2a028d9e61a44d55e33af5f72bf27c3435fc352d
Joerg Schneider <joerg.schneider@z-lab.com>
2019-03-14 09:30:31 +0100

    fix(core): ZDS-202 enabled npmPublish in release step adjusted commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-case": [2, "always", "upper-case"],
+    "scope-case": [
+      1,
+      "always",
+      "upper-case",
+      "lower-case",
+      "pascal-case",
+      "start-case"
+    ],
     "subject-case": [1, "always", ["sentence-case", "lower-case", "start-case"]]
   }
 };
