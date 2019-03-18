30a485b150b843fb5e47d425e66cdba854a82504
Joerg Schneider <joerg.schneider@z-lab.com>
2019-03-18 18:29:04 +0100

    chore(styles): try theming solution for button - WIP

--- src/elements/button/buttonData.js
+++ src/elements/button/buttonData.js
@@ -1,23 +1,37 @@
-module.exports = {
+import * as helpers from '../../helpers';
+
+const buttonData = {
   text: {
     name: 'text',
-    js: `<button type="button" class="zep-button" data-zep-type="button">Text Button</button>`,
+    js: `<button type="button" class="${helpers.classPrefix}button" ${
+      helpers.htmlDataVarType
+    }="button">Text Button</button>`,
     react: false,
     angular: false,
     vue: false
   },
   filled: {
     name: 'filled',
-    js: `<button type="button" class="zep-button zep-button__filled" data-zep-type="button">Filled Button</button>`,
+    js: `<button type="button" class="${helpers.classPrefix}button ${
+      helpers.classPrefix
+    }button__filled" ${
+      helpers.htmlDataVarType
+    }="button">Filled Button</button>`,
     react: false,
     angular: false,
     vue: false
   },
   contained: {
     name: 'contained',
-    js: `<button type="button" class="zep-button zep-button__contained" data-zep-type="button">Contained Button</button>`,
+    js: `<button type="button" class="${helpers.classPrefix}button ${
+      helpers.classPrefix
+    }button__contained" ${
+      helpers.htmlDataVarType
+    }="button">Contained Button</button>`,
     react: false,
     angular: false,
     vue: false
   }
 };
+
+export default buttonData;
