3b246c91472620bbeef7bafda1aab6fd078d701f
Sebastian Bartus-Kunz <sebastian.bartus-kunz@z-lab.com>
2019-04-08 19:25:53 +0200

    fix(icons): Rename icons

--- bundlehandler.sh
+++ bundlehandler.sh
@@ -13,22 +13,29 @@ TARGET_FILE="bundle/zeppelin-element-library.css"
 find build/static/css -name \*.css -exec cp {} $TARGET_FILE \;
 
 # copy fonts into bundle folder
-cp -r src/assets/fonts/* bundle/assets/fonts 
+cp -r src/assets/fonts/* bundle/assets/fonts
 
 # copy theme folder into bundle folder
 cp -r src/themes/* bundle/themes
 
 # copy icons folder into bundle folder
 cp -r src/assets/icons/* bundle/assets/icons
-# for f in src/assets/icons/*; do cp -- "$f" "${bundle/assets/icons/}zepicons-$f" ; done
 cd bundle/assets/icons
-find . -name 'Icons*.svg' -type f -exec bash -c 'mv "$1" "${1/\/IconsLanguages//}"' -- {} \;
-find . -name 'Icons*.svg' -type f -exec bash -c 'mv "$1" "${1/\/IconsMiscAction//}"' -- {} \;
-find . -name 'Icons*.svg' -type f -exec bash -c 'mv "$1" "${1/\/IconsMiscNavigation//}"' -- {} \;
-find . -name 'Icons*.svg' -type f -exec bash -c 'mv "$1" "${1/\/IconsMiscIndicator//}"' -- {} \;
-find . -name 'Icons*.svg' -type f -exec bash -c 'mv "$1" "${1/\/IconsSbu//}"' -- {} \;
-find . -name 'Icons*.svg' -type f -exec bash -c 'mv "$1" "${1/\/IconsSocial//}"' -- {} \;
-for f in * ; do mv -- "$f" "Zepicons$f" ; done
+
+# replace IconsMiscActionAccount.svg with zep-icons-account
+for f in *.svg
+do
+    newFileName=$(echo $f | \
+     awk '{gsub(/IconsLanguages/, "Icons")};1' | \
+     awk '{gsub(/IconsMiscAction/, "Icons")};1' | \
+     awk '{gsub(/IconsMiscNavigation/, "Icons")};1' | \
+     awk '{gsub(/IconsMiscIndicator/, "Icons")};1' | \
+     awk '{gsub(/IconsSbu/, "Icons")};1' | \
+     awk '{gsub(/IconsSocial/, "Icons")};1' | \
+     awk '{gsub(/[A-Z]/, "-&")};1' | \
+     awk '{ print tolower($0) }')
+    mv $f "zep$newFileName"
+done
 
 cd ../../../
 
@@ -42,9 +49,9 @@ rm $TARGET_FILE.bak
 
 # delete hash from font name in css file
 for font in eot woff woff2 ttf svg
-do 
+do
     sed -i.bak 's/\(\.\)\(.[^\.]*\)\(\.'$font'\)/\3/g' $TARGET_FILE
-    
+
 done
 rm $TARGET_FILE.bak
 
