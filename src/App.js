b62632135431d501a8fdb13e1e1599d4bd6a93b8
Joerg Schneider <joerg.schneider@z-lab.com>
2019-04-15 17:05:11 +0200

    feat(layout): added basic flexbox grid
    added basic flexbox grid system with float grid fallback for IE9

--- src/App.js
+++ src/App.js
@@ -26,6 +26,40 @@ function App() {
         </div>
       </div>
       <div className="zep-grid">
+        <div className="zep-grid__col zep-grid__col--1">
+          <button className="zel-button zel-button-primary">test 1</button>
+        </div>
+        <div className="zep-grid__col zep-grid__col--2">
+          <button className="zel-button zel-button-primary">test 2</button>
+        </div>
+        <div className="zep-grid__col zep-grid__col--3">
+          <button className="zel-button zel-button-primary">test 3</button>
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--1">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 1
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--2">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 2
+              </button>
+            </div>
+          ))}
+        </div>
         <div className="zep-grid__row">
           {Object.keys(elements).map((elem, i) => (
             <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--3">
@@ -33,11 +67,120 @@ function App() {
                 className="zel-button zel-button-primary"
                 onClick={() => setContent(elements[elem].name)}
               >
-                {elements[elem].name}
+                {elements[elem].name} 3
               </button>
             </div>
-         ))}
+          ))}
         </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--4">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 4
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--5">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 5
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--6">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 6
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--7">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 7
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--8">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 8
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--9">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 9
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--10">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 10
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--11">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 11
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="zep-grid__row">
+          {Object.keys(elements).map((elem, i) => (
+            <div key={`btn-${i}`} className="zep-grid__col zep-grid__col--12">
+              <button
+                className="zel-button zel-button-primary"
+                onClick={() => setContent(elements[elem].name)}
+              >
+                {elements[elem].name} 12
+              </button>
+            </div>
+          ))}
+        </div>
+
         <div style={{ flexGrow: 1, padding: 45 }}>
           {content === 'welcome' && (
             <p>Welcome to the Zeppelin Element Library</p>
--- src/elements/elements.js
+++ src/elements/elements.js
@@ -10,5 +10,41 @@ module.exports = {
   tag: {
     name: 'tag',
     isReady: true
+  },
+  button1: {
+    name: 'button',
+    isReady: true
+  },
+  tabs1: {
+    name: 'tabs',
+    isReady: false
+  },
+  tag1: {
+    name: 'tag',
+    isReady: true
+  },
+  button2: {
+    name: 'button',
+    isReady: true
+  },
+  tabs2: {
+    name: 'tabs',
+    isReady: false
+  },
+  tag2: {
+    name: 'tag',
+    isReady: true
+  },
+  button3: {
+    name: 'button',
+    isReady: true
+  },
+  tabs3: {
+    name: 'tabs',
+    isReady: false
+  },
+  tag3: {
+    name: 'tag',
+    isReady: true
   }
 };
