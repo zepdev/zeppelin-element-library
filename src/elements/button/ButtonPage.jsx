6e579cce8576367a089fa96e5793d92b3dc63041
Joerg Schneider <joerg.schneider@z-lab.com>
2019-04-16 13:58:37 +0200

    refactor(layout): grid improvements
    improved grid, added gutter and edited demo page for testing

--- src/elements/button/ButtonPage.jsx
+++ src/elements/button/ButtonPage.jsx
@@ -8,14 +8,15 @@ function ButtonPage() {
   });
 
   return (
-    <>
+    <div className="zep-grid__row">
       {Object.keys(data).map((elem, idx) => (
         <span
+          className="zep-grid__col zep-grid__col--12-12"
           dangerouslySetInnerHTML={{ __html: data[elem].js }}
           key={`button${idx}`}
         />
       ))}
-    </>
+    </div>
   );
 }
 
