670cc1d5c12fda677bc3945a416a8706e6f2a398
Sebastian Bartus-Kunz <sebastian.bartus-kunz@z-lab.com>
2019-05-06 16:26:38 +0200

    fix(input): WIP: floating labels

--- src/elements/input/InputPage.jsx
+++ src/elements/input/InputPage.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 function Input() {
+  var themes = [
+    { key: 'zeppelin', name: 'Zeppelin' },
+    { key: 'cat', name: 'CAT' },
+    { key: 'rental', name: 'Rental' }
+  ];
   return (
     <>
-      <input type="text" className="zep-input" defaultValue="Type Something" />
+      {themes.map((theme, key) => {
+        return (
+          <div
+            className={'theme-' + theme.key + ' zep-form-container'}
+            style={{ marginBottom: 15 }}
+            key={key}
+          >
+            <label
+              className="zep-form-container__label"
+              htmlFor={'input-' + theme.key}
+            >
+              Input {theme.name}
+            </label>
+            <input
+              type="text"
+              className="zep-input"
+              placeholder={'Input ' + theme.name}
+              id={'input-' + theme.key}
+            />
+          </div>
+        );
+      })}
     </>
   );
 }
