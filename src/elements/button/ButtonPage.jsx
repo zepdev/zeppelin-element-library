76d293626bb423c97c4954fecedc45ee524a586e
Alyssa <ca.butler10@gmail.com>
2019-05-08 09:32:37 +0200

    fix(button): fixed width of icon button

--- src/elements/button/ButtonPage.jsx
+++ src/elements/button/ButtonPage.jsx
@@ -7,56 +7,50 @@ function ButtonPage() {
       <>
         <p>Primary - Large</p>
         <div className="theme-zeppelin" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-primary">
+          <button className="zep-button zep-button-primary">
             <span className="zep-button__text">Normal button</span>
           </button>
         </div>
         <div className="theme-cat" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-primary">
+          <button className="zep-button zep-button-primary">
             <span className="zep-button__text">Normal button</span>
           </button>
         </div>
         <div className="theme-rental" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-primary">
+          <button className="zep-button zep-button-primary">
             <span className="zep-button__text">Normal button</span>
           </button>
         </div>
         <p>Other</p>
         <div className="theme-zeppelin" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-primary zep-button--small"
-          >
+          <button className="zep-button zep-button-primary zep-button--small">
             <span className="zep-button__text">SMALL button</span>
           </button>
         </div>
         <div className="theme-zeppelin" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-primary zep-button-full"
-          >
+          <button className="zep-button zep-button-primary zep-button--full">
             <span className="zep-button__text">Full Width button</span>
           </button>
         </div>
         <p>Secondary</p>
         <div className="theme-zeppelin" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-secondary">
+          <button className="zep-button zep-button-secondary">
             <span className="zep-button__text">Normal button</span>
           </button>
         </div>
         <div className="theme-cat" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-secondary">
+          <button className="zep-button zep-button-secondary">
             <span className="zep-button__text">Normal button</span>
           </button>
         </div>
         <div className="theme-rental" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-secondary">
+          <button className="zep-button zep-button-secondary">
             <span className="zep-button__text">Normal button</span>
           </button>
         </div>
         <p>Tertiary</p>
         <div className="theme-zeppelin" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-tertiary">
+          <button className="zep-button zep-button-tertiary">
             <CopyIcon className="zep-button__icon zep-button__icon--tertiary" />
             <span className="zep-button__text zep-button__text--tertiary">
               Icon button
@@ -64,7 +58,7 @@ function ButtonPage() {
           </button>
         </div>
         <div className="theme-cat" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-tertiary">
+          <button className="zep-button zep-button-tertiary">
             <CopyIcon className="zep-button__icon zep-button__icon--tertiary" />
             <span className="zep-button__text zep-button__text--tertiary">
               Icon button
@@ -72,7 +66,7 @@ function ButtonPage() {
           </button>
         </div>
         <div className="theme-rental" style={{ marginBottom: 15 }}>
-          <button type="button" className="zep-button zep-button-tertiary">
+          <button className="zep-button zep-button-tertiary">
             <CopyIcon className="zep-button__icon zep-button__icon--tertiary" />
             <span className="zep-button__text zep-button__text--tertiary">
               Icon button
@@ -81,50 +75,32 @@ function ButtonPage() {
         </div>
         <p>Icon</p>
         <div className="theme-cat" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-primary zep-button-icon"
-          >
+          <button className="zep-button zep-button-primary zep-button-icon">
             <CopyIcon className="zep-button__icon" />
           </button>
         </div>
         <div className="theme-zeppelin" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-primary zep-button-icon"
-          >
+          <button className="zep-button zep-button-primary zep-button-icon">
             <CopyIcon className="zep-button__icon" />
           </button>
         </div>
         <div className="theme-rental" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-primary zep-button-icon"
-          >
+          <button className="zep-button zep-button-primary zep-button-icon">
             <CopyIcon className="zep-button__icon" />
           </button>
         </div>
         <div className="theme-cat" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-secondary zep-button-icon"
-          >
+          <button className="zep-button zep-button-secondary zep-button-icon">
             <CopyIcon className="zep-button__icon" />
           </button>
         </div>
         <div className="theme-zeppelin" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-secondary zep-button-icon"
-          >
+          <button className="zep-button zep-button-secondary zep-button-icon">
             <CopyIcon className="zep-button__icon" />
           </button>
         </div>
         <div className="theme-rental" style={{ marginBottom: 15 }}>
-          <button
-            type="button"
-            className="zep-button zep-button-secondary zep-button-icon"
-          >
+          <button className="zep-button zep-button-secondary zep-button-icon">
             <CopyIcon className="zep-button__icon" />
           </button>
         </div>
