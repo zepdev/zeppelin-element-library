408fe28d4e8bf5eba78301ab28c304500ac24f73
Alyssa <ca.butler10@gmail.com>
2019-05-23 08:50:10 +0200

    refactor(tabs): accessibility added

--- src/elements/tabs/TabsPage.jsx
+++ src/elements/tabs/TabsPage.jsx
@@ -7,26 +7,91 @@ function TabsPage() {
     <>
       <p>Big</p>
       {themes.map(elem => (
-        <div className={`theme-${elem} zep-tabs`} key={`tabs${elem}`}>
-          <div className="zep-tab">Tab 1</div>
-          <div className="zep-tab">Tab 2</div>
+        <div
+          className={`theme-${elem} zep-tabs`}
+          key={`tabs${elem}`}
+          role="tablist"
+          aria-label={`tabs ${elem}`}
+        >
+          <button
+            className="zep-tab"
+            role="tab"
+            aria-selected="true"
+            aria-controls="panel-1"
+            id="tab-1"
+            tabindex="0"
+          >
+            Tab 1
+          </button>
+          <button
+            className="zep-tab"
+            role="tab"
+            aria-selected="false"
+            aria-controls="panel-2"
+            id="tab-2"
+            tabindex="-1"
+          >
+            Tab 2
+          </button>
         </div>
       ))}
       <p>Small</p>
       {themes.map(elem => (
-        <div className={`theme-${elem} zep-tabs`} key={`tabs${elem}`}>
-          <div className="zep-tab zep-tab--small">Tab 1</div>
-          <div className="zep-tab zep-tab--small">Tab 2</div>
+        <div
+          className={`theme-${elem} zep-tabs`}
+          key={`tabs${elem}`}
+          role="tablist"
+          aria-label={`tabs ${elem}`}
+        >
+          <div
+            className="zep-tab zep-tab--small"
+            role="tab"
+            aria-selected="true"
+            aria-controls="panel-1"
+            id="tab-1"
+            tabindex="0"
+          >
+            Tab 1
+          </div>
+          <div
+            className="zep-tab zep-tab--small"
+            role="tab"
+            aria-selected="false"
+            aria-controls="panel-2"
+            id="tab-2"
+            tabindex="-1"
+          >
+            Tab 2
+          </div>
         </div>
       ))}
       <p>With Icon</p>
       {themes.map(elem => (
-        <div className={`theme-${elem} zep-tabs`} key={`tabs${elem}`}>
-          <div className="zep-tab zep-tab--icon">
+        <div
+          className={`theme-${elem} zep-tabs`}
+          key={`tabs${elem}`}
+          role="tablist"
+          aria-label={`tabs ${elem}`}
+        >
+          <div
+            className="zep-tab zep-tab--icon"
+            role="tab"
+            aria-selected="true"
+            aria-controls="panel-1"
+            id="tab-1"
+            tabindex="0"
+          >
             <Icon icon="zepicons-calendar-range" className="zep-tab__icon" />
             Tab 1
           </div>
-          <div className="zep-tab zep-tab--icon">
+          <div
+            className="zep-tab zep-tab--icon"
+            role="tab"
+            aria-selected="false"
+            aria-controls="panel-2"
+            id="tab-2"
+            tabindex="-1"
+          >
             <Icon icon="zepicons-calendar-range" className="zep-tab__icon" />
             Tab 2
           </div>
