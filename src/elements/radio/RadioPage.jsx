91ac193aff9a0b90be92ea3e0ba92dfefa3308ed
Alyssa <ca.butler10@gmail.com>
2020-03-24 15:43:17 +0100

    fix(checkbox): fixed border on active

--- src/elements/radio/RadioPage.jsx
+++ src/elements/radio/RadioPage.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Radio from './Radio';
+import RadioGroup from './RadioGroup';
 
 function RadioPage() {
   const themes = ['indigo', 'yellow', 'red'];
+  const [radio, setRadio] = useState('a');
+  function handleChange(event) {
+    setRadio(event.target.value);
+    console.log(event.target.value);
+  }
+  console.log(radio);
   return (
     <>
       {themes.map(elem => (
@@ -21,6 +29,31 @@ function RadioPage() {
           </label>
         </div>
       ))}
+      <div className="theme-indigo">
+        <RadioGroup title="Select One">
+          <Radio
+            value="a"
+            id="radioA"
+            name="value 1"
+            checked={radio === 'a'}
+            onChange={handleChange}
+          />
+          <Radio
+            value="b"
+            name="value 2"
+            id="radioB"
+            checked={radio === 'b'}
+            onChange={handleChange}
+          />
+          <Radio
+            value="c"
+            name="value 3"
+            id="radioC"
+            checked={radio === 'c'}
+            onChange={handleChange}
+          />
+        </RadioGroup>
+      </div>
     </>
   );
 }
