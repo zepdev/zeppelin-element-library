800924a87c36480869e38384c41bc04e1b5ecb42
Joerg Schneider <joerg.schneider@zeppelin.com>
2020-12-10 13:56:48 +0100

    refactor(button): use new typography scss for icon buttons and pagination buttons

--- src/elements/button/ButtonPage.jsx
+++ src/elements/button/ButtonPage.jsx
@@ -1,215 +1,163 @@
 import React from 'react';
-import CopyIcon from '../icon/CopyIcon';
 import Icon from '../icon/Icon';
 
 function ButtonPage() {
-  const theme = ['indigo', 'yellow', 'red', 'blue'];
+  const themes = ['none', 'indigo', 'yellow', 'red', 'blue'];
   const sizes = ['', 'medium', 'small'];
+  const types = ['primary', 'secondary'];
   return (
     <>
-      <p style={{ marginBottom: 15 }} className="zep-typo--headline-3">
-        Primary
-      </p>
-      <p className="zep-typo--headline-5" style={{ marginBottom: 15 }}>
-        Primary (anchor tags)
-      </p>
-      {theme.map((elem) => (
-        <div
-          className={`theme-${elem}`}
-          style={{ marginBottom: 15 }}
-          key={elem}
-        >
-          {sizes.map((elem2) => (
-            <a
+      {types.map((type) => (
+        <div key={type} style={{ marginBottom: 50 }}>
+          <p style={{ marginBottom: 15 }} className="zep-typo--headline-3">
+            {type.toUpperCase()}
+          </p>
+          <p className="zep-typo--headline-5" style={{ marginBottom: 15 }}>
+            {type.toUpperCase()} (anchor tags)
+          </p>
+          {themes.map((theme) => (
+            <div
+              className={`theme-${theme}`}
+              style={{ marginBottom: 15 }}
+              key={theme}
+            >
+              <p
+                className="zep-typo--display-default"
+                style={{ marginBottom: 5 }}
+              >
+                Theme {theme}
+              </p>
+              {sizes.map((size) => (
+                <a
+                  href="https://www.zeppelin.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`zep-button zep-button--${type} zep-button--${size}`}
+                  style={{ margin: 4 }}
+                  key={size}
+                >
+                  {type}
+                </a>
+              ))}
+            </div>
+          ))}
+          <div style={{ marginBottom: 15 }}>
+            <p
+              className="zep-typo--display-default"
+              style={{ marginBottom: 10 }}
+            >
+              disabled
+            </p>
+            {sizes.map((size2) => (
+              <a
+                href="https://www.zeppelin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`zep-button zep-button--${type} zep-button--${size2} zep-button-${type}--disabled`}
+                style={{ margin: 4 }}
+                key={size2}
+              >
+                {type}
+              </a>
+            ))}
+          </div>
+          <p className="zep-typo--headline-5" style={{ marginBottom: 15 }}>
+            {type.toUpperCase()} (button tags)
+          </p>
+          {themes.map((theme2) => (
+            <div
+              className={`theme-${theme2}`}
+              style={{ marginBottom: 15 }}
+              key={theme2}
+            >
+              <p
+                className="zep-typo--display-default"
+                style={{ marginBottom: 5 }}
+              >
+                Theme {theme2}
+              </p>
+              {sizes.map((size3) => (
+                <button
+                  className={`zep-button zep-button--${type} zep-button--${size3}`}
+                  style={{ margin: 4 }}
+                  key={size3}
+                >
+                  {type}
+                </button>
+              ))}
+            </div>
+          ))}
+          <div style={{ marginBottom: 15 }}>
+            <p
+              className="zep-typo--display-default"
+              style={{ marginBottom: 10 }}
+            >
+              disabled
+            </p>
+            {sizes.map((size4) => (
+              <button
+                className={`zep-button zep-button--${type} zep-button--${size4}`}
+                style={{ margin: 4 }}
+                key={size4}
+                disabled
+              >
+                {type}
+              </button>
+            ))}
+          </div>
+          <p className="zep-typo--headline-5" style={{ marginBottom: 15 }}>
+            {type.toUpperCase()} full width
+          </p>
+          <div className="theme-indigo" style={{ marginBottom: 15 }}>
+            <button
               href="https://www.zeppelin.com"
               target="_blank"
               rel="noopener noreferrer"
-              className={`zep-button-primary zep-button--${elem2}`}
-              style={{ margin: 4 }}
-              key={elem2}
+              className={`zep-button zep-button--${type} zep-button--full`}
             >
-              Primary
-            </a>
-          ))}
-        </div>
-      ))}
-      <div style={{ marginBottom: 15 }}>
-        {sizes.map((elem2) => (
-          <a
-            href="https://www.zeppelin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`zep-button-primary zep-button--${elem2} zep-button-primary--disabled`}
-            style={{ margin: 4 }}
-            key={elem2}
-          >
-            Primary
-          </a>
-        ))}
-      </div>
-      <p className="zep-typo--headline-5">Primary (button tags)</p>
-      {theme.map((elem) => (
-        <div
-          className={`theme-${elem}`}
-          style={{ marginBottom: 15 }}
-          key={elem}
-        >
-          {sizes.map((elem2) => (
-            <button
-              className={`zep-button-primary zep-button--${elem2}`}
-              style={{ margin: 4 }}
-              key={elem2}
-            >
-              Primary
+              Full Width button (anchor tag)
             </button>
-          ))}
-        </div>
-      ))}
-      <div style={{ marginBottom: 15 }}>
-        {sizes.map((elem2) => (
-          <button
-            className={`zep-button-primary zep-button--${elem2}`}
-            style={{ margin: 4 }}
-            key={elem2}
-            disabled
-          >
-            Primary
-          </button>
-        ))}
-      </div>
-      <p className="zep-typo--headline-5" style={{ marginBottom: 15 }}>
-        Primary full width
-      </p>
-      <div className="theme-indigo" style={{ marginBottom: 15 }}>
-        <button
-          href="https://www.zeppelin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="zep-button-primary zep-button--full"
-        >
-          Full Width button (anchor tag)
-        </button>
-      </div>
-      <div className="theme-indigo" style={{ marginBottom: 15 }}>
-        <button className="zep-button-primary zep-button--full">
-          Full Width
-        </button>
-      </div>
-      <p
-        style={{ marginBottom: 15, marginTop: 50 }}
-        className="zep-typo--headline-3"
-      >
-        Secondary
-      </p>
-      <p style={{ marginBottom: 15 }} className="zep-typo--headline-5">
-        Secondary (anchor tags)
-      </p>
-      {theme.map((elem) => (
-        <div
-          className={`theme-${elem}`}
-          style={{ marginBottom: 15 }}
-          key={elem}
-        >
-          {sizes.map((elem2) => (
-            <a
-              href="https://www.zeppelin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`zep-button-secondary zep-button--${elem2}`}
-              style={{ margin: 4 }}
-              key={elem2}
-            >
-              Secondary
-            </a>
-          ))}
-        </div>
-      ))}
-      <div style={{ marginBottom: 15 }}>
-        {sizes.map((elem2) => (
-          <a
-            href="https://www.zeppelin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`zep-button-secondary zep-button--${elem2} zep-button-secondary--disabled`}
-            style={{ margin: 4 }}
-            key={elem2}
-          >
-            Secondary
-          </a>
-        ))}
-      </div>
-      <p className="zep-typo--headline-5">Secondary (button tags)</p>
-      {theme.map((elem) => (
-        <div
-          className={`theme-${elem}`}
-          style={{ marginBottom: 15 }}
-          key={elem}
-        >
-          {sizes.map((elem2) => (
+          </div>
+          <div className="theme-indigo" style={{ marginBottom: 15 }}>
             <button
-              className={`zep-button-secondary zep-button--${elem2}`}
-              style={{ margin: 4 }}
-              key={elem2}
+              className={`zep-button zep-button--${type} zep-button--full`}
             >
-              Secondary
+              Full Width
             </button>
+          </div>
+          <p style={{ marginBottom: 15 }} className="zep-typo--headline-5">
+            Icon {type.toUpperCase()}
+          </p>
+          {themes.map((theme3) => (
+            <div
+              className={`theme-${theme3}`}
+              style={{ marginBottom: 15 }}
+              key={theme3}
+            >
+              <p
+                className="zep-typo--display-default"
+                style={{ marginBottom: 5 }}
+              >
+                Theme {theme3}
+              </p>
+              <button
+                className={`zep-button zep-button--${type} zep-button-icon`}
+              >
+                <Icon icon="zepicons-copy" className="zep-button__icon" />
+              </button>
+            </div>
           ))}
-        </div>
-      ))}
-      <div style={{ marginBottom: 15 }}>
-        {sizes.map((elem2) => (
-          <button
-            className={`zep-button-secondary zep-button--${elem2}`}
-            style={{ margin: 4 }}
-            key={elem2}
+          <p className="zep-typo--display-default" style={{ marginBottom: 10 }}>
             disabled
-          >
-            Secondary
-          </button>
-        ))}
-      </div>
-      <p className="zep-typo--headline-5">Secondary full width</p>
-      <div className="theme-indigo" style={{ marginBottom: 15 }}>
-        <button
-          href="https://www.zeppelin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="zep-button-secondary zep-button--full"
-        >
-          Full Width button (anchor tag)
-        </button>
-      </div>
-      <div className="theme-indigo" style={{ marginBottom: 15 }}>
-        <button className="zep-button-secondary zep-button--full">
-          Full Width
-        </button>
-      </div>
-      <p
-        style={{ marginBottom: 15, marginTop: 50 }}
-        className="zep-typo--headline-3"
-      >
-        Icon
-      </p>
-      {theme.map((elem) => (
-        <div
-          className={`theme-${elem}`}
-          style={{ marginBottom: 15 }}
-          key={elem}
-        >
-          <button className="zep-button zep-button-primary zep-button-icon">
-            <Icon icon="zepicons-copy" className="zep-button__icon" />
-          </button>
-        </div>
-      ))}
-      {theme.map((elem) => (
-        <div
-          className={`theme-${elem}`}
-          style={{ marginBottom: 15 }}
-          key={elem}
-        >
-          <button className="zep-button zep-button-secondary zep-button-icon">
-            <CopyIcon className="zep-button__icon" />
-          </button>
+          </p>
+          <div style={{ marginBottom: 15 }}>
+            <button
+              className={`zep-button zep-button--${type} zep-button-icon`}
+              disabled
+            >
+              <Icon icon="zepicons-copy" className="zep-button__icon" />
+            </button>
+          </div>
         </div>
       ))}
     </>
