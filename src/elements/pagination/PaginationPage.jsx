ee63167c725df2c273ba838f7c1817975241df97
Joerg Schneider <joerg.schneider@zeppelin.com>
2020-12-10 15:12:40 +0100

    refactor(pagination): add selected element styles to paginationPage

--- src/elements/pagination/PaginationPage.jsx
+++ src/elements/pagination/PaginationPage.jsx
@@ -5,32 +5,41 @@ function PaginationPage() {
   const themes = ['indigo', 'yellow', 'red'];
   return (
     <>
-      {themes.map((elem) => (
-        <div
-          className={`theme-${elem} zep-pagination`}
-          style={{ marginBottom: 15 }}
-          key={`pagination${elem}`}
-        >
-          <button className="zep-button zep-button-icon" disabled>
-            <Icon icon="zepicons-previous-page" className="zep-button__icon" />
-          </button>
-          <button className="zep-button zep-button-icon" disabled>
-            <Icon icon="zepicons-chevron-left" className="zep-button__icon" />
-          </button>
-          {[1, 2, 3, 4, 5].map((elem) => (
-            <button
-              className="zep-button zep-button-pagination"
-              key={`count${elem}`}
-            >
-              {elem}
+      {themes.map((elem, i) => (
+        <div key={`pagination${elem}`} style={{ marginBottom: 15 }}>
+          <p style={{ marginBottom: 5 }} className="zep-typo--body-default">
+            Theme {elem} (selected item: {i + 1})
+          </p>
+          <div className={`theme-${elem} zep-pagination`}>
+            <button className="zep-button zep-button--icon" disabled>
+              <Icon
+                icon="zepicons-previous-page"
+                className="zep-button__icon"
+              />
             </button>
-          ))}
-          <button className="zep-button zep-button-icon">
-            <Icon icon="zepicons-chevron-right" className="zep-button__icon" />
-          </button>
-          <button className="zep-button zep-button-icon">
-            <Icon icon="zepicons-next-page" className="zep-button__icon" />
-          </button>
+            <button className="zep-button zep-button--icon" disabled>
+              <Icon icon="zepicons-chevron-left" className="zep-button__icon" />
+            </button>
+            {[1, 2, 3, 4, 5].map((num) => (
+              <button
+                className={`zep-button zep-button--pagination ${
+                  num === i + 1 ? 'zep-button--selected' : ''
+                }`}
+                key={`count${num}`}
+              >
+                {num}
+              </button>
+            ))}
+            <button className="zep-button zep-button--icon">
+              <Icon
+                icon="zepicons-chevron-right"
+                className="zep-button__icon"
+              />
+            </button>
+            <button className="zep-button zep-button--icon">
+              <Icon icon="zepicons-next-page" className="zep-button__icon" />
+            </button>
+          </div>
         </div>
       ))}
     </>
